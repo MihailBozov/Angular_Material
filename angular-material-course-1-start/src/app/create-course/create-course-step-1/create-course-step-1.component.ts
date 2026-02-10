331e333b70cd3e18aa741ecd0ad5474f2c280a99
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-02-10 20:42:16 +0200

    Text Area

--- angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ angular-material-course-1-start/src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,13 +1,15 @@
-import {Component} from '@angular/core';
-import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
 import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
 
+const SAMPLE_TEXT = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
 @Component({
-    selector: "create-course-step-1",
-    templateUrl: "create-course-step-1.component.html",
-    styleUrls: ["create-course-step-1.component.scss"],
-    standalone: false
+  selector: "create-course-step-1",
+  templateUrl: "create-course-step-1.component.html",
+  styleUrls: ["create-course-step-1.component.scss"],
+  standalone: false
 })
 export class CreateCourseStep1Component {
 
@@ -17,23 +19,23 @@ export class CreateCourseStep1Component {
       Validators.minLength(5),
       Validators.maxLength(60)
     ]],
-    releasedAt: [new Date(1990,0,1), Validators.required],
+    releasedAt: [new Date(1990, 0, 1), Validators.required],
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
-  
+
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     const date = cellDate.getDate();
-    
-    if(view === 'month') {
+
+    if (view === 'month') {
       return (date == 1) ? `highlight-date` : '';
     }
     return '';
   }
-  
-  
+
+
 
   constructor(private fb: UntypedFormBuilder) {
 
