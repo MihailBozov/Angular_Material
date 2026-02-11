e8d1561e2838d53d252dde2f57e62253f1a59a39
Mihail Bozov <129510773+MihailBozov@users.noreply.github.com>
2026-02-11 22:22:06 +0200

    Paginator

--- angular-material-course-1-start/src/app/course/course.component.ts
+++ angular-material-course-1-start/src/app/course/course.component.ts
@@ -5,7 +5,7 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import { Course } from "../model/course";
 import { CoursesService } from "../services/courses.service";
-import { debounceTime, distinctUntilChanged, startWith, tap, delay, catchError } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize } from 'rxjs/operators';
 import { merge, fromEvent, throwError } from "rxjs";
 import { Lesson } from '../model/lesson';
 
@@ -19,8 +19,11 @@ import { Lesson } from '../model/lesson';
 export class CourseComponent implements OnInit, AfterViewInit {
 
   course: Course;
-
   lessons: Lesson[] = [];
+  loading = false;
+  
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
   constructor(private route: ActivatedRoute,
     private coursesService: CoursesService) {
@@ -39,14 +42,21 @@ export class CourseComponent implements OnInit, AfterViewInit {
   }
 
   loadLessonsPage() {
+    this.loading = true;
     this.coursesService
-      .findLessons(this.course.id, "asc", 0, 3)
+      .findLessons(this.course.id, 
+        "asc", 
+        this.paginator?.pageIndex ?? 0, 
+        this.paginator?.pageSize ?? 3)
       .pipe(
         tap(lessons => this.lessons = lessons),
         catchError(err => {
           console.log("Error loading lessons.", err)
           alert("Error loading lessons.");
           return throwError(err);
+        }),
+        finalize(() => {
+          this.loading = false;
         })
       )
       .subscribe();
@@ -55,7 +65,11 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
 
-
+    this.paginator.page
+    .pipe(
+      tap(() => this.loadLessonsPage())
+    )
+    .subscribe();
   }
 
 }
